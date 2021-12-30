9b0a960e7664f50c1e0474546ce1f6c0d8eac10d
marceldybek <dybek8780@gmail.com>
2021-12-30 18:01:04 +0100

    poprawiona lista elektrowni

--- src/com/company/Head/Serializacja.java
+++ src/com/company/Head/Serializacja.java
@@ -1,35 +1,61 @@
 package com.company.Head;
 
+import com.company.Elektrownie.Elektrownia;
 import java.io.*;
+import java.util.List;
 
 public class Serializacja {
 
-    public static void zapis(Gracz gracz) {
-        try(ObjectOutputStream so = new ObjectOutputStream(new FileOutputStream("PlikAktualnySave.ser"))) {
+    public void zapis(List<Elektrownia> listaElektrowniArrayList) {
+        try(ObjectOutputStream so = new ObjectOutputStream(new FileOutputStream("ElektrownieGracza.ser"))) {
 
-            so.writeObject(gracz);
-
+            so.writeObject(listaElektrowniArrayList);
         }
         catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static Gracz odczyt() {
+    public List<Elektrownia> odczyt() {
 
 
-        try (ObjectInputStream is = new ObjectInputStream(new FileInputStream("PlikAktualnySave.ser"))) {
+        try (ObjectInputStream is = new ObjectInputStream(new FileInputStream("ElektrownieGracza.ser"))) {
 
             Object obj1 = is.readObject();
-
-
-            return (Gracz) obj1;
-
+            is.close();
+            return (List<Elektrownia>) obj1;
 
         }
         catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
             return null;
+        }
+
+    }
+
+    public void zapisStanuKonta(float stan) {
+        try(ObjectOutputStream so = new ObjectOutputStream(new FileOutputStream("StanKontaGracza.ser"))) {
+
+            so.writeObject(stan);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public float odczytStanuKonta() {
+
+
+        try (ObjectInputStream is = new ObjectInputStream(new FileInputStream("StanKontaGracza.ser"))) {
+
+            Object obj1 = is.readObject();
+            is.close();
+            return (float) obj1;
+
+        }
+        catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return 0;
         }
 
     }
