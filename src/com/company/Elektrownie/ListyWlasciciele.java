f0b8610852d2fef045c85b966978a979f964972c
krubik66 <krubik66@gmail.com>
2022-01-04 18:03:58 +0100

    Wlasciciele nazwy

--- src/com/company/Elektrownie/ListyWlasciciele.java
+++ src/com/company/Elektrownie/ListyWlasciciele.java
@@ -12,8 +12,13 @@ public class ListyWlasciciele  implements Serializable {
             add( new Wlasciciel("NajtanszyOdbiorSmieci", "PL83762346", "Karol Wiśnia"));
             add( new Wlasciciel("Promyk", "PL46372837", "Kamil Glik"));
             add( new Wlasciciel("FirmaGracza","123456789","PodajNick"));
-            add( new Wlasciciel());
-            add( new Wlasciciel());
+            add( new Wlasciciel("Wiatr","brak","Thor"));
+            add( new Wlasciciel("Xiong","56754","Xiao Shen"));
+            add( new Wlasciciel("Continental","3465734","Pavel"));
+            add( new Wlasciciel("Tauron","PL4457537","Kowalski"));
+            add( new Wlasciciel("Rosija","999999999","Nikotiv"));
+            add( new Wlasciciel("Baguette","176482358","Dubois"));
+            add( new Wlasciciel("Gewurst","45682358","Karl"));
         }
     };
 
