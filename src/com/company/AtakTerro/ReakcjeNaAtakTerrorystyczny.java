31c9ca46846d299367a3a127e653bc84daae9cb8
RESP0 <96990446+RESP0@users.noreply.github.com>
2022-01-04 17:38:50 +0100

    Update ReakcjeNaAtakTerrorystyczny.java

--- src/com/company/AtakTerro/ReakcjeNaAtakTerrorystyczny.java
+++ src/com/company/AtakTerro/ReakcjeNaAtakTerrorystyczny.java
@@ -1,10 +1,11 @@
 package com.company.AtakTerro;
 
 import com.company.Elektrownie.Elektrownia;
+import com.company.Head.Gracz;
 
 public interface ReakcjeNaAtakTerrorystyczny  {
 
-    void ReakcjaNaAtakTerrorystyczny(Elektrownia e);
+    void ReakcjaNaAtakTerrorystyczny(Elektrownia e, Gracz g);
 }
 
 
